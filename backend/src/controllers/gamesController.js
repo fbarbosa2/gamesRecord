fdea41a9d6a41844fd98f99b2ce923a3df055a1a
Francisco Barbosa <f_barbosa@outlook.pt>
2025-02-09 12:22:06 +0000

    update gamesController

--- backend/src/controllers/gamesController.js
+++ backend/src/controllers/gamesController.js
@@ -13,7 +13,7 @@ const searchGame = async (req, res) => {
             params: {
                 key: API_KEY,
                 search: search || '',
-                page_size: 10, // Number of results per page
+                page_size: 15, // Number of results per page
             }
         });
 
